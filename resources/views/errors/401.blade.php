a5b8444b74fe974ca9148b3211b0b06df83746bb
Yustinus Windy <54971881+windy1989@users.noreply.github.com>
2024-08-21 05:30:58 +0700

    Update 401.blade.php

--- resources/views/errors/401.blade.php
+++ resources/views/errors/401.blade.php
@@ -28,7 +28,7 @@
 
 </head>
 
-<body>
+<body class="vh-100">
     <div class="authincation h-100">
         <div class="container h-100">
             <div class="row justify-content-center h-100 align-items-center">

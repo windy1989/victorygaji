d67b4fda9a572f0d95802534927025e2b13a92af
Yustinus Windy <54971881+windy1989@users.noreply.github.com>
2024-08-21 05:25:53 +0700

    Update AdminAuth.php

--- app/Http/Middleware/AdminAuth.php
+++ app/Http/Middleware/AdminAuth.php
@@ -17,24 +17,23 @@ class AdminAuth
     public function handle(Request $request, Closure $next, $role1 = null, $role2 = null, $role3 = null, $role4 = null)
     {
         $passed = false;
-        $arrayAccess = ['1','2','3','4','5','6','7','8'];
         if($role1){
-            if(in_array($role1,$arrayAccess)){
+            if($role1 == session('bo_type')){
                 $passed = true;
             }
         }
         if($role2){
-            if(in_array($role2,$arrayAccess)){
+            if($role2 == session('bo_type')){
                 $passed = true;
             }
         }
         if($role3){
-            if(in_array($role3,$arrayAccess)){
+            if($role3 == session('bo_type')){
                 $passed = true;
             }
         }
         if($role4){
-            if(in_array($role4,$arrayAccess)){
+            if($role4 == session('bo_type')){
                 $passed = true;
             }
         }

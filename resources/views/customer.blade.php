70562f590f69a2136322ad5b328f2319b86f5e4d
Yustinus Windy <54971881+windy1989@users.noreply.github.com>
2024-08-22 05:14:58 +0700

    Update customer.blade.php

--- resources/views/customer.blade.php
+++ resources/views/customer.blade.php
@@ -76,7 +76,81 @@
                         </button>
                     </div>
                     <div class="modal-body">
-					
+                        <div id="validation_alert" style="display:none;margin-top:25px;"></div>
+                        <form>
+                            <div class="row">
+                                <div class="mb-3 col-md-6">
+                                    <label class="form-label">Kode Pelanggan</label>
+                                    <input type="hidden" id="temp" name="temp">
+                                    <input type="text" class="form-control" placeholder="Kode Pelanggan" id="code" name="code">
+                                </div>
+                                <div class="mb-3 col-md-6">
+                                    <label class="form-label">Nama</label>
+                                    <input type="text" class="form-control" placeholder="Nama" id="name" name="name">
+                                </div>
+                                <div class="mb-3 col-md-6">
+                                    <label class="form-label">Email</label>
+                                    <input type="email" class="form-control" placeholder="Email" id="email" name="name">
+                                </div>
+                                <div class="mb-3 col-md-6">
+                                    <label class="form-label">Nama Pemilik</label>
+                                    <input type="text" class="form-control" placeholder="Nama Pemilik" id="owner_name" name="owner_name">
+                                </div>
+                                <div class="mb-3 col-md-6">
+                                    <label class="form-label">Nama PIC</label>
+                                    <input type="text" class="form-control" placeholder="Nama PIC" id="pic" name="pic">
+                                </div>
+                                <div class="mb-3 col-md-6">
+                                    <label class="form-label">Perusahaan</label>
+                                    <input type="text" class="form-control" placeholder="Perusahaan" id="company_name" name="company_name">
+                                </div>
+                                <div class="mb-3 col-md-6">
+                                    <label class="form-label">No. Akta Pendirian</label>
+                                    <input type="text" class="form-control" placeholder="No. Akta Pendirian" id="document_no" name="document_no">
+                                </div>
+                                <div class="mb-3 col-md-6">
+                                    <label class="form-label">Alamat</label>
+                                    <input type="text" class="form-control" placeholder="Alamat" id="address" name="address">
+                                </div>
+                                <div class="mb-3 col-md-6">
+                                    <label class="form-label">Kota</label>
+                                    <input type="text" class="form-control" placeholder="Alamat" id="city" name="city">
+                                </div>
+                                <div class="mb-3 col-md-6">
+                                    <label class="form-label">Gender</label>
+                                    <input type="text" class="form-control" placeholder="Alamat" id="city" name="city">
+                                </div>
+                                <div class="mb-3 col-md-6 mb-0">
+                                    <label class="radio-inline me-3"><input type="radio" name="optradio" class="form-check-input"> Option 1</label>
+                                    <label class="radio-inline me-3"><input type="radio" name="optradio" class="form-check-input"> Option 2</label>
+                                    <label class="radio-inline me-3"><input type="radio" name="optradio" class="form-check-input"> Option 3</label>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="mb-3 col-md-6">
+                                    <label class="form-label">State</label>
+                                    <select id="inputState" class="default-select form-control wide">
+                                        <option selected>Choose...</option>
+                                        <option>Option 1</option>
+                                        <option>Option 2</option>
+                                        <option>Option 3</option>
+                                    </select>
+                                </div>
+                                <div class="mb-3 col-md-6">
+                                    <label class="form-label">Zip</label>
+                                    <input type="text" class="form-control">
+                                </div>
+                            </div>
+                            <div class="mb-3">
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox">
+                                    <label class="form-check-label">
+                                        Check me out
+                                    </label>
+                                </div>
+                            </div>
+                            <button type="submit" class="btn btn-primary">Sign in</button>
+                        </form>
                     </div>
                     <div class="modal-footer">
                         <button type="button" class="btn btn-danger light" data-bs-dismiss="modal">Close</button>

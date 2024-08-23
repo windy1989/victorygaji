78b704354d74970ce45f42c212ba513e7f572ec0
Yustinus Windy <54971881+windy1989@users.noreply.github.com>
2024-08-24 05:37:58 +0700

    gas

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -42,7 +42,7 @@
 
     <script src="{{ url('assets/js/custom.min.js') }}"></script>
 	<script src="{{ url('assets/js/dlabnav-init.js') }}"></script>
-	<script src="{{ url('assets/js/all.js?v=31') }}"></script>
+	<script src="{{ url('assets/js/all.js?v=32') }}"></script>
   
 </body>
 </html>

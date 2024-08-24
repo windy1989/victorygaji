bb7d2cfd421741e8a14ce4150977b92cb9fbdcc7
Yustinus Windy <54971881+windy1989@users.noreply.github.com>
2024-08-24 15:07:00 +0700

    Update footer.blade.php

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -42,7 +42,7 @@
 
     <script src="{{ url('assets/js/custom.min.js') }}"></script>
 	<script src="{{ url('assets/js/dlabnav-init.js') }}"></script>
-	<script src="{{ url('assets/js/all.js?v=51') }}"></script>
+	<script src="{{ url('assets/js/all.js?v=53') }}"></script>
   
 </body>
 </html>

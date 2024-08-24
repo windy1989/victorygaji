79503e5ca06837db9edca8b128f17fc6e156a22d
Yustinus Windy <54971881+windy1989@users.noreply.github.com>
2024-08-24 15:58:29 +0700

    gas

--- public/assets/js/all.js
+++ public/assets/js/all.js
@@ -661,7 +661,7 @@ function save(){
         closeOnConfirm: !1,
         closeOnCancel: !1
     }).then(function (willDelete) {
-        if (willDelete) {
+        if (willDelete.value) {
             var formData = new FormData($('#formData')[0]);
             $.ajax({
                 url: window.location.href + '/create',

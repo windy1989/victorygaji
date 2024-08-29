e21b3624d54b10c76ec2793813dabfef85fc69d8
Yustinus Windy <54971881+windy1989@users.noreply.github.com>
2024-08-29 21:02:36 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CustomerController;
+use App\Http\Controllers\ProjectController;
 
 /*
 |--------------------------------------------------------------------------

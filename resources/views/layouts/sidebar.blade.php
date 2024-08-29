a33b43c66756df28fe3b451f22cd9d83174cab0f
Yustinus Windy <54971881+windy1989@users.noreply.github.com>
2024-08-29 21:11:26 +0700

    Update sidebar.blade.php

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -94,7 +94,7 @@
 							<i class="flaticon-045-heart"></i>
 							<span class="nav-text">Plugins</span>
 						</a>
-						<ul aria-expanded="false">
+						<ul aria-expanded="false" class="{{ Request::segment(2) == 'customer' ? 'left mm-collapse mm-show' : '' }}">
 							<li><a href="uc-select2.html">Select 2</a></li>
 							<li><a href="uc-nestable.html">Nestedable</a></li>
 							<li><a href="uc-noui-slider.html">Noui Slider</a></li>

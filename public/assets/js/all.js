a6ca570b3e093c53924b697d5f1e4f3cf8ce485d
Yustinus Windy <54971881+windy1989@users.noreply.github.com>
2024-08-31 22:29:16 +0700

    gas

--- public/assets/js/all.js
+++ public/assets/js/all.js
@@ -255,8 +255,8 @@ $(function() {
             save();
         }
     } );
-    jQuery('#modalCreate').on('shown.bs.modal', function() {
-        jQuery(document).off('focusin.modal');
+    $('#modalCreate').on('shown.bs.modal', function() {
+        $(document).off('focusin.modal');
     });
 });
 

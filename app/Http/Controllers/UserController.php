48f1f1251433f5a10a2c5dbf8e88873c8429c7a5
Yustinus Windy <54971881+windy1989@users.noreply.github.com>
2024-09-03 04:47:37 +0700

    Update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -80,7 +80,7 @@ class UserController extends Controller
                     '
                         <a href="javascript:void(0);" class="btn btn-secondary btn-sm content-icon" onclick="updatePassword('.$val->id.')"><i class="fa fa-unlock"></i></a>
                         <a href="javascript:void(0);" class="btn btn-warning btn-sm content-icon" onclick="edit('.$val->id.')"><i class="fa fa-edit"></i></a>
-                        <a href="javascript:void(0);" class="btn btn-danger btn-sm content-icon" onclick="destroy('.$val->id.')"><i class="fa fa-times"></i></a>
+                        <a href="javascript:void(0);" class="btn btn-danger btn-sm content-icon" onclick="destroy('.$val->id.')"><i class="fa fa-trash"></i></a>
 					'
                 ];
 

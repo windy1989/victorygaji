f66337baf142c849d62818a1b4f05de639510f84
Yustinus Windy <54971881+windy1989@users.noreply.github.com>
2024-09-11 05:13:03 +0700

    gas

--- public/assets/js/all.js
+++ public/assets/js/all.js
@@ -289,22 +289,24 @@ $(function() {
     });
     if($('#fileReceipt').length > 0){
         $("#fileReceipt").on('change', function () {
-            if (typeof (FileReader) != "undefined") {
-                var image_holder = $("#previewFileReceipt");
-                image_holder.empty();
+            if($('#fileReceipt').val()){
+                if (typeof (FileReader) != "undefined") {
+                    var image_holder = $("#previewFileReceipt");
+                    image_holder.empty();
 
-                var reader = new FileReader();
-                reader.onload = function (e) {
-                    $("<img />", {
-                        "src": e.target.result,
-                        "class": "thumb-image",
-                        "width": "100%"
-                    }).appendTo(image_holder);
-                };
-                image_holder.show();
-                reader.readAsDataURL($(this)[0].files[0]);
-            } else {
-                alert("This browser does not support FileReader.");
+                    var reader = new FileReader();
+                    reader.onload = function (e) {
+                        $("<img />", {
+                            "src": e.target.result,
+                            "class": "thumb-image",
+                            "width": "100%"
+                        }).appendTo(image_holder);
+                    };
+                    image_holder.show();
+                    reader.readAsDataURL($(this)[0].files[0]);
+                } else {
+                    alert("This browser does not support FileReader.");
+                }
             }
         });
     }
--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -42,7 +42,7 @@
 
     <script src="{{ url('assets/js/custom.min.js') }}"></script>
 	<script src="{{ url('assets/js/dlabnav-init.js') }}"></script>
-	<script src="{{ url('assets/js/all.js?v=111') }}"></script>
+	<script src="{{ url('assets/js/all.js?v=112') }}"></script>
   
 </body>
 </html>

454f6ea4f4412e205ccc9b15c88fffb41b15a2a2
Yustinus Windy <54971881+windy1989@users.noreply.github.com>
2024-09-14 11:19:00 +0700

    Update Approval.php

--- app/Models/Approval.php
+++ app/Models/Approval.php
@@ -6,10 +6,11 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Approval extends Model
 {
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, SoftDeletes;
 
     protected $table = 'approvals';
     protected $primaryKey = 'id';

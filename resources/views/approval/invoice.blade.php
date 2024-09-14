44bcf31a37272fdbfbd8d29fc3f9af719ae63c9b
Yustinus Windy <54971881+windy1989@users.noreply.github.com>
2024-09-14 13:09:28 +0700

    Update invoice.blade.php

--- resources/views/approval/invoice.blade.php
+++ resources/views/approval/invoice.blade.php
@@ -30,74 +30,11 @@
                                 <h4 class="card-title">Daftar {{ $title }}</h4>
                             </div>
                             <div class="card-body">
-                                <div class="col-xl-4">
-                                    <div class="row">
-                                        <div class="col-xl-12">
-                                            <div class="card">
-                                                <div class="card-body">
-                                                    <div class="profile-statistics">
-                                                        <div class="text-center">
-                                                            <div class="row">
-                                                                <div class="col">
-                                                                    <h3 class="m-b-0">150</h3><span>Follower</span>
-                                                                </div>
-                                                                <div class="col">
-                                                                    <h3 class="m-b-0">140</h3><span>Place Stay</span>
-                                                                </div>
-                                                                <div class="col">
-                                                                    <h3 class="m-b-0">45</h3><span>Reviews</span>
-                                                                </div>
-                                                            </div>
-                                                            <div class="mt-4">
-                                                                <a href="javascript:void(0);" class="btn btn-primary mb-1 me-1">Follow</a> 
-                                                                <a href="javascript:void(0);" class="btn btn-primary mb-1" data-bs-toggle="modal" data-bs-target="#sendMessageModal">Send Message</a>
-                                                            </div>
-                                                        </div>
-                                                        <!-- Modal -->
-                                                        <div class="modal fade" id="sendMessageModal">
-                                                            <div class="modal-dialog modal-dialog-centered" role="document">
-                                                                <div class="modal-content">
-                                                                    <div class="modal-header">
-                                                                        <h5 class="modal-title">Send Message</h5>
-                                                                        <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
-                                                                    </div>
-                                                                    <div class="modal-body">
-                                                                        <form class="comment-form">
-                                                                            <div class="row"> 
-                                                                                <div class="col-lg-6">
-                                                                                    <div class="mb-3">
-                                                                                        <label class="text-black font-w600 form-label">Name <span class="required">*</span></label>
-                                                                                        <input type="text" class="form-control" value="Author" name="Author" placeholder="Author">
-                                                                                    </div>
-                                                                                </div>
-                                                                                <div class="col-lg-6">
-                                                                                    <div class="mb-3">
-                                                                                        <label class="text-black font-w600 form-label">Email <span class="required">*</span></label>
-                                                                                        <input type="text" class="form-control" value="Email" placeholder="Email" name="Email">
-                                                                                    </div>
-                                                                                </div>
-                                                                                <div class="col-lg-12">
-                                                                                    <div class="mb-3">
-                                                                                        <label class="text-black font-w600 form-label">Comment</label>
-                                                                                        <textarea rows="8" class="form-control" name="comment" placeholder="Comment"></textarea>
-                                                                                    </div>
-                                                                                </div>
-                                                                                <div class="col-lg-12">
-                                                                                    <div class="mb-3 mb-0">
-                                                                                        <input type="submit" value="Post Comment" class="submit btn btn-primary" name="submit">
-                                                                                    </div>
-                                                                                </div>
-                                                                            </div>
-                                                                        </form>
-                                                                    </div>
-                                                                </div>
-                                                            </div>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
+                                <div class="profile-uoloaded-post border-bottom-1 pb-5">
+                                    <img src="images/profile/8.jpg" alt="" class="img-fluid w-100 rounded">
+                                    <h3 class="text-black">Collection of textile samples lay spread</h3>
+                                    <button class="btn btn-primary me-2"><span class="me-2"><i class="fa fa-heart"></i></span>Like</button>
+                                    <button class="btn btn-secondary"  data-bs-toggle="modal" data-bs-target="#replyModal"><span class="me-2"><i class="fa fa-reply"></i></span>Reply</button>
                                 </div>
                             </div>
                         </div>

ed7e36d8f5214787f9da0d84cac2dc3aa841298c
Yustinus Windy <54971881+windy1989@users.noreply.github.com>
2024-09-19 21:25:48 +0700

    Update invoice.blade.php

---
+++ resources/views/pdf/invoice.blade.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<title>{{ $title }}</title>
+		<style>
+			body {
+				font-family:"Calibri", sans-serif;
+			}			
+		</style>
+	</head>
+	<body>
+		<div class="invoice-box">
+			<table cellpadding="0" cellspacing="0" width="100%">
+				<tr>
+					<td>
+						<table>
+							<tr>
+								<td style="text-align:center;">
+									<img src="{{ url('assets/images/square_logo.png') }}" width="auto" height="75px">
+								</td>
+							</tr>
+                            <tr>
+								<td style="text-align:center;">
+									<h3>{{ $title }}</h3>
+								</td>
+							</tr>
+						</table>
+					</td>
+				</tr>
+			</table>
+		</div>
+	</body>
+</html>

57566083c65c4865f086002d91fc1703e5712405
Yustinus Windy <54971881+windy1989@users.noreply.github.com>
2024-09-19 21:55:44 +0700

    Update invoice.blade.php

--- resources/views/pdf/invoice.blade.php
+++ resources/views/pdf/invoice.blade.php
@@ -10,7 +10,7 @@
                 font-size:12px;
             }
             th, td {
-                padding: 0px;
+                padding: 0px !important;
             }
             @page { margin: 0.5cm; }
 		</style>

6f0646cf6c2c8160b7d4de557ce0002d5991e3e6
Yustinus Windy <54971881+windy1989@users.noreply.github.com>
2024-09-21 12:33:39 +0700

    Update invoice.blade.php

--- resources/views/pdf/invoice.blade.php
+++ resources/views/pdf/invoice.blade.php
@@ -157,7 +157,7 @@
                 </tfoot>
 			</table>
             <br>
-            <div style="margin-left:50px;">
+            <div style="margin-left:50px;font-size:14px;">
                 Keterangan :
                 <p>
                     Adapun pembayaran mohon di transfer ke nomor rekening berikut :<br>

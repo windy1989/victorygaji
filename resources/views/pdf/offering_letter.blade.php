978b4305668072b80232ae016f49aa43e2a973a3
Yustinus Windy <54971881+windy1989@users.noreply.github.com>
2024-09-25 20:12:46 +0700

    Update offering_letter.blade.php

--- resources/views/pdf/offering_letter.blade.php
+++ resources/views/pdf/offering_letter.blade.php
@@ -184,6 +184,6 @@
                 </tbody>
 			</table>
 		</div>
-        <img src="{{ public_path('storage/promotion/offering.png') }}" width="80%" height="auto" style="margin-left:50px;">
+        <img src="{{ public_path('storage/promotion/offering.png') }}" width="80%" height="auto" style="margin-left:75px;">
 	</body>
 </html>

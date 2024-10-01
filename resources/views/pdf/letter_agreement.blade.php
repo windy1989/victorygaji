61a12f2dc4231993c832c4234f2caea76b36e334
Yustinus Windy <54971881+windy1989@users.noreply.github.com>
2024-10-01 13:46:47 +0700

    Update letter_agreement.blade.php

--- resources/views/pdf/letter_agreement.blade.php
+++ resources/views/pdf/letter_agreement.blade.php
@@ -64,7 +64,7 @@
                         
                     </td>
                     <td width="25%">
-                        <div style="border:1px solid black;width:125px;height:125px;text-align:center;vertical-align:middle;">
+                        <div style="border:1px solid black;width:100px;height:100px;text-align:center;vertical-align:middle;">
                             KOP PERUSAHAAN
                         </div>
                     </td>

6762dbb73e7857f11c08aa302662b94477ba403d
Yustinus Windy <windykuro@gmail.com>
2024-10-06 20:36:41 +0700

    Update letter_agreement.blade.php

--- resources/views/pdf/letter_agreement.blade.php
+++ resources/views/pdf/letter_agreement.blade.php
@@ -34,12 +34,12 @@
             }
             
             .table-content {
-                font-size:15px;
+                font-size:13px;
                 line-height: 1.8;
             }
 
             .table-content-body {
-                font-size:15px;
+                font-size:13px;
                 line-height: 1.8;
                 text-align: justify;
             }
@@ -79,14 +79,14 @@
                     </td>
                 </tr>
 			</table>
-            <table class="table-content" cellpadding="0" cellspacing="0" width="100%" style="margin:auto;">
+            <table class="table-content" cellpadding="0" cellspacing="0" width="90%" style="margin:auto;">
 				<tr>
                     <td width="100%" style="text-align:justify;">
                         Pada hari ini <b><i>{{ CustomHelper::hari($data->post_date) }}</i></b> Tanggal <b><i>{{ date('j',strtotime($data->post_date)) }} ( {{ CustomHelper::terbilang(date('j',strtotime($data->post_date))) }} )</i></b> Bulan <b><i>{{ CustomHelper::bulan($data->post_date) }}</i></b> tahun <b><i>{{ date('Y',strtotime($data->post_date)) }}</i></b>, yang bertanda tangan dibawah ini masing - masing
                     </td>
                 </tr>
 			</table>
-            <table class="table-content" cellpadding="0" cellspacing="0" width="100%" style="margin:auto;text-align:justify;">
+            <table class="table-content" cellpadding="0" cellspacing="0" width="90%" style="margin:auto;text-align:justify;">
 				<tr>
                     <td width="20%">
                         Nama

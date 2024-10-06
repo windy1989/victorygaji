4d5634ec9f3d093a75a96a43fd642ecbd8cc3ad9
Yustinus Windy <windykuro@gmail.com>
2024-10-06 22:44:29 +0700

    Update letter_agreement.blade.php

--- resources/views/pdf/letter_agreement.blade.php
+++ resources/views/pdf/letter_agreement.blade.php
@@ -55,7 +55,7 @@
             header { position: fixed; top: -135px; left: 0px; right: 0px; height: 150px; margin-bottom: 10em }
 
             li {
-                break-inside: auto;
+                page-break-inside: auto;
             }
 		</style>
 	</head>

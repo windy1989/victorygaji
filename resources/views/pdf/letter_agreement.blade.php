5d24cf8ab5f05f973a6f2ffec9d625e4379e5ddd
Yustinus Windy <windykuro@gmail.com>
2024-10-08 00:02:43 +0700

    Update letter_agreement.blade.php

--- resources/views/pdf/letter_agreement.blade.php
+++ resources/views/pdf/letter_agreement.blade.php
@@ -608,5 +608,19 @@
                 </table>
             </div>
         </main>
+        <script type="text/php">
+            if (isset($pdf)) {
+                $x = 250;
+                $y = 10;
+                $text = "Page {PAGE_NUM} of {PAGE_COUNT}";
+                $font = null;
+                $size = 14;
+                $color = array(255,0,0);
+                $word_space = 0.0;  //  default
+                $char_space = 0.0;  //  default
+                $angle = 0.0;   //  default
+                $pdf->page_text($x, $y, $text, $font, $size, $color, $word_space, $char_space, $angle);
+            }
+        </script>
 	</body>
 </html>

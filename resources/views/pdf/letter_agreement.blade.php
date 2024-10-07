156a5265ada452e0816436f8258dd21c558f379e
Yustinus Windy <windykuro@gmail.com>
2024-10-07 23:51:34 +0700

    Update letter_agreement.blade.php

--- resources/views/pdf/letter_agreement.blade.php
+++ resources/views/pdf/letter_agreement.blade.php
@@ -605,21 +605,4 @@
             </div>
         </main>
 	</body>
-    <script type="text/php"> 
-    
-        if (isset($pdf)) { 
-         //Shows number center-bottom of A4 page with $x,$y values
-            $x = 250;  //X-axis i.e. vertical position 
-            $y = 820; //Y-axis horizontal position
-            $text = "Page {PAGE_NUM} of {PAGE_COUNT}";  //format of display message
-            $font =  $fontMetrics->get_font("helvetica", "bold");
-            $size = 10;
-            $color = array(255,0,0);
-            $word_space = 0.0;  //  default
-            $char_space = 0.0;  //  default
-            $angle = 0.0;   //  default
-            $pdf->page_text($x, $y, $text, $font, $size, $color, $word_space, $char_space, $angle);
-        }
-        
-    </script>
 </html>

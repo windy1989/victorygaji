3bfd66f46a38ead519197418ea3007984926ee49
Yustinus Windy <windykuro@gmail.com>
2024-10-13 20:20:14 +0700

    asd

--- public/assets/js/all.js
+++ public/assets/js/all.js
@@ -1025,9 +1025,8 @@ function showUpload(code){
         success: function(response) {
             if(response.status == 200){
                 $('#modal-detail-title-upload').text(response.code);
-
+                $("#list-files").empty();
                 if(response.data.length > 0){
-                    $("#list-files").empty();
                     $.each(response.data, function(i, val) {
                         $('#list-files').append(`
                             <div class="col-md-2">
@@ -1036,7 +1035,16 @@ function showUpload(code){
                         `);
                     });
                 }else{
-
+                    $('#list-images').append(`
+						<div class="col-md-12 text-center">
+							<div class="alert alert-warning solid alert-dismissible fade show">
+                                <svg viewBox="0 0 24 24" width="24" height="24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" class="me-2"><path d="M10.29 3.86L1.82 18a2 2 0 0 0 1.71 3h16.94a2 2 0 0 0 1.71-3L13.71 3.86a2 2 0 0 0-3.42 0z"></path><line x1="12" y1="9" x2="12" y2="13"></line><line x1="12" y1="17" x2="12.01" y2="17"></line></svg>
+                                <strong>Warning!</strong> Data file lama tidak tersedia.
+                                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="btn-close">
+                                </button>
+                            </div>
+						</div>
+					`);
                 }                
 
                 $('#modalUpload').modal('toggle');

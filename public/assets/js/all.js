1f388c973b33568b3791e87119b623140d5a5798
Yustinus Windy <windykuro@gmail.com>
2024-10-13 20:20:52 +0700

    asd

--- public/assets/js/all.js
+++ public/assets/js/all.js
@@ -1035,7 +1035,7 @@ function showUpload(code){
                         `);
                     });
                 }else{
-                    $('#list-images').append(`
+                    $('#list-files').append(`
 						<div class="col-md-12 text-center">
 							<div class="alert alert-warning solid alert-dismissible fade show">
                                 <svg viewBox="0 0 24 24" width="24" height="24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" class="me-2"><path d="M10.29 3.86L1.82 18a2 2 0 0 0 1.71 3h16.94a2 2 0 0 0 1.71-3L13.71 3.86a2 2 0 0 0-3.42 0z"></path><line x1="12" y1="9" x2="12" y2="13"></line><line x1="12" y1="17" x2="12.01" y2="17"></line></svg>
--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -42,7 +42,7 @@
 
     <script src="{{ url('assets/js/custom.min.js') }}"></script>
 	<script src="{{ url('assets/js/dlabnav-init.js') }}"></script>
-	<script src="{{ url('assets/js/all.js?v=151') }}"></script>
+	<script src="{{ url('assets/js/all.js?v=152') }}"></script>
   
 </body>
 </html>

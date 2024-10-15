379923f64b85c77df18095bba79ed815f7cc075b
Yustinus Windy <windykuro@gmail.com>
2024-10-15 22:23:28 +0700

    asd

--- public/assets/js/all.js
+++ public/assets/js/all.js
@@ -106,7 +106,13 @@ Dropzone.options.dropzoneUploadSmall = {
                     </div>    
                 `);
                 successMessage(responseText.message);
-                loadDataTableSurveyResult();
+                if($('#survey-result-datatable').length > 0){
+                    loadDataTableSurveyResult();
+                }
+                if($('#survey-item-datatable').length > 0){
+                    loadDataTableSurveyItem();
+                }
+                $('#no-file-error').remove();
             }else if(responseText.status == '422'){
                 $('#validation_alert_upload').show();
                 $.each(responseText.error, function(i, val) {
@@ -1174,7 +1180,7 @@ function showUpload(code){
                     });
                 }else{
                     $('#list-files').append(`
-						<div class="col-md-12 text-center">
+						<div class="col-md-12 text-center" id="no-file-error">
 							<div class="alert alert-warning solid alert-dismissible fade show">
                                 <svg viewBox="0 0 24 24" width="24" height="24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" class="me-2"><path d="M10.29 3.86L1.82 18a2 2 0 0 0 1.71 3h16.94a2 2 0 0 0 1.71-3L13.71 3.86a2 2 0 0 0-3.42 0z"></path><line x1="12" y1="9" x2="12" y2="13"></line><line x1="12" y1="17" x2="12.01" y2="17"></line></svg>
                                 <strong>Warning!</strong> Data file lama tidak tersedia.

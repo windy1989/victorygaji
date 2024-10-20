80de4b8c8d2d6b06159941d843294d2d5f379a25
Yustinus Windy <windykuro@gmail.com>
2024-10-20 20:47:48 +0700

    Update document.blade.php

--- resources/views/document.blade.php
+++ resources/views/document.blade.php
@@ -146,3 +146,21 @@
                 </div>
             </div>
         </div>
+
+        <div class="modal fade" id="modalDetail">
+            <div class="modal-dialog modal-lg" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title">Detail Informasi <b id="modal-detail-title"></b></h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal">
+                        </button>
+                    </div>
+                    <div class="modal-body" id="modal-detail-body">
+                        
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-danger light" data-bs-dismiss="modal">Close</button>
+                    </div>
+                </div>
+            </div>
+        </div>

64705d539033e3cf4fdb4c4202aadcb50739c4a8
Yustinus Windy <windykuro@gmail.com>
2024-10-27 16:46:49 +0700

    asd

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -35,7 +35,7 @@
                                 </a>
 							</li>
 							<li class="nav-item dropdown notification_dropdown">
-                                <a class="nav-link" href="javascript:void(0);" role="button" data-bs-toggle="dropdown" onclick="seeNotif();">
+                                <a class="nav-link" href="javascript:void(0);" role="button" data-bs-toggle="dropdown" onclick="/* seeNotif(); */">
                                   <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
 									  <g  data-name="Layer 2" transform="translate(-2 -2)">
 										<path id="Path_20" data-name="Path 20" d="M22.571,15.8V13.066a8.5,8.5,0,0,0-7.714-8.455V2.857a.857.857,0,0,0-1.714,0V4.611a8.5,8.5,0,0,0-7.714,8.455V15.8A4.293,4.293,0,0,0,2,20a2.574,2.574,0,0,0,2.571,2.571H9.8a4.286,4.286,0,0,0,8.4,0h5.23A2.574,2.574,0,0,0,26,20,4.293,4.293,0,0,0,22.571,15.8ZM7.143,13.066a6.789,6.789,0,0,1,6.78-6.78h.154a6.789,6.789,0,0,1,6.78,6.78v2.649H7.143ZM14,24.286a2.567,2.567,0,0,1-2.413-1.714h4.827A2.567,2.567,0,0,1,14,24.286Zm9.429-3.429H4.571A.858.858,0,0,1,3.714,20a2.574,2.574,0,0,1,2.571-2.571H21.714A2.574,2.574,0,0,1,24.286,20a.858.858,0,0,1-.857.857Z"/>

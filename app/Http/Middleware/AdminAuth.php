6da536d2280f4dc53588d8220beb0a4c5d82b394
Yustinus Windy <windykuro@gmail.com>
2024-10-31 05:05:23 +0700

    asd

--- app/Http/Middleware/AdminAuth.php
+++ app/Http/Middleware/AdminAuth.php
@@ -14,7 +14,7 @@ class AdminAuth
      * @param  \Closure  $next
      * @return mixed
      */
-    public function handle(Request $request, Closure $next, $role1 = null, $role2 = null, $role3 = null, $role4 = null)
+    public function handle(Request $request, Closure $next, $role1 = null, $role2 = null, $role3 = null, $role4 = null, $role5 = null, $role6 = null, $role7 = null, $role8 = null, $role9 = null, $role10 = null): mixed
     {
         $passed = false;
         if($role1){
@@ -34,6 +34,36 @@ class AdminAuth
         }
         if($role4){
             if($role4 == session('bo_type')){
+                $passed = true;
+            }
+        }
+        if($role5){
+            if($role5 == session('bo_type')){
+                $passed = true;
+            }
+        }
+        if($role6){
+            if($role6 == session('bo_type')){
+                $passed = true;
+            }
+        }
+        if($role7){
+            if($role7 == session('bo_type')){
+                $passed = true;
+            }
+        }
+        if($role8){
+            if($role8 == session('bo_type')){
+                $passed = true;
+            }
+        }
+        if($role9){
+            if($role9 == session('bo_type')){
+                $passed = true;
+            }
+        }
+        if($role10){
+            if($role10 == session('bo_type')){
                 $passed = true;
             }
         }

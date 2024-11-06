6dc2239d3fcb75fe14e676c2bce4313a9aad1e87
Yustinus Windy <windykuro@gmail.com>
2024-11-07 04:04:47 +0700

    Update letter_agreement.blade.php

--- resources/views/pdf/letter_agreement.blade.php
+++ resources/views/pdf/letter_agreement.blade.php
@@ -506,7 +506,6 @@
                         </ol>
                     </p>
                 </div>
-                <br>
                 <div style="margin:auto;width:90%;text-align:justify !important;">
                     <p style="text-align:center;"><b>Pasal 6</b></p>
                     <p style="text-align:center;">FORCE MAJEURE</p>

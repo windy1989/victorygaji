46b7ca76519ea13dea782a56648660498ee80ab1
Yustinus Windy <windykuro@gmail.com>
2024-11-09 12:06:42 +0700

    Update invoice.blade.php

--- resources/views/invoice.blade.php
+++ resources/views/invoice.blade.php
@@ -242,4 +242,21 @@
                     (grandtotal >= 0 ? '' : '-') + formatRupiahIni(grandtotal.toFixed(2).toString().replace('.',','))
                 );
             }
+
+            function formatRupiahIni(angka){
+                var number_string = angka.toString().replace(/[^,\d]/g, '').toString(),
+                split   		= number_string.split(','),
+                sisa     		= split[0].length % 3,
+                rupiah     		= split[0].substr(0, sisa),
+                ribuan     		= split[0].substr(sisa).match(/\d{3}/gi);
+
+                if(ribuan){
+                    separator = sisa ? '.' : '';
+                    rupiah += separator + ribuan.join('.');
+                }
+
+                rupiah = split[1] != undefined ? rupiah + ',' + split[1] : rupiah;
+
+                return rupiah;
+            }
         </script>

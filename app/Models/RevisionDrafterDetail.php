3abac29b4bc2a1bb5f209a4796ca11692d14d775
Yustinus Windy <windykuro@gmail.com>
2024-11-21 09:27:54 +0700

    update

--- app/Models/RevisionDrafterDetail.php
+++ app/Models/RevisionDrafterDetail.php
@@ -51,6 +51,8 @@ class RevisionDrafterDetail extends Model
                 $document = '<a href="'.asset(Storage::url($this->file_location)).'" target="_blank"><img src="'.asset('assets/images/docx.png').'" style="max-height:100px;"></a>';
             }elseif(explode('.',$this->file_location)[1] == 'pdf'){
                 $document = '<a href="'.asset(Storage::url($this->file_location)).'" target="_blank"><img src="'.asset('assets/images/pdf.png').'" style="max-height:100px;"></a>';
+            }elseif(in_array(explode('.',$this->file_location)[1],['xls','xlsx'])){
+                $document = '<a href="'.asset(Storage::url($this->file_location)).'" target="_blank"><img src="'.asset('assets/images/excel.png').'" style="max-height:100px;"></a>';
             }else{ 
                 $document = '<img src="'.asset(Storage::url($this->file_location)).'" style="max-height:100px;">';
             }
--- app/Models/SurveyResultDetail.php
+++ app/Models/SurveyResultDetail.php
@@ -49,6 +49,8 @@ class SurveyResultDetail extends Model
         if(Storage::exists($this->file_location)) {
             if(explode('.',$this->file_location)[1] == 'pdf'){
                 $document = '<a href="'.asset(Storage::url($this->file_location)).'" target="_blank"><img src="'.asset('assets/images/pdf.png').'" style="max-height:100px;"></a>';
+            }elseif(in_array(explode('.',$this->file_location)[1],['xls','xlsx'])){
+                $document = '<a href="'.asset(Storage::url($this->file_location)).'" target="_blank"><img src="'.asset('assets/images/excel.png').'" style="max-height:100px;"></a>';
             }else{ 
                 $document = '<img src="'.asset(Storage::url($this->file_location)).'" style="max-height:100px;">';
             }

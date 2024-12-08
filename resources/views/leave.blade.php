478f623ec1cd2fddd541b034517bca042d9c6d52
Yustinus Windy <windykuro@gmail.com>
2024-12-08 21:12:00 +0700

    Update leave.blade.php

--- resources/views/leave.blade.php
+++ resources/views/leave.blade.php
@@ -37,7 +37,7 @@
                             </div>
                             <div class="card-body">
                                 <div class="table-responsive">
-                                    <table id="offering-letter-datatable" class="display nowrap" style="min-width: 100%">
+                                    <table id="leave-datatable" class="display nowrap" style="min-width: 100%">
                                         <thead>
                                             <tr>
                                                 <th>#</th>

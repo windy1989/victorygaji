83b093e1909936930a4259706d421dfa77699d5e
Yustinus Windy <windykuro@gmail.com>
2024-12-13 20:09:35 +0700

    Update leave.blade.php

--- resources/views/leave.blade.php
+++ resources/views/leave.blade.php
@@ -36,12 +36,12 @@
                                 </div>
                             </div>
                             <div class="card-body">
-                                <div class="alert alert-warning solid alert-dismissible fade show">
+                                {{-- <div class="alert alert-warning solid alert-dismissible fade show">
 									<svg viewBox="0 0 24 24" width="24" height="24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" class="me-2"><path d="M10.29 3.86L1.82 18a2 2 0 0 0 1.71 3h16.94a2 2 0 0 0 1.71-3L13.71 3.86a2 2 0 0 0-3.42 0z"></path><line x1="12" y1="9" x2="12" y2="13"></line><line x1="12" y1="17" x2="12.01" y2="17"></line></svg>
 									<strong>INFO!</strong> UNDER MAINTENANCE.
 									<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="btn-close">
                                     </button>
-								</div>
+								</div> --}}
                                 <div class="table-responsive">
                                     <table id="leave-datatable" class="display nowrap" style="min-width: 100%">
                                         <thead>
